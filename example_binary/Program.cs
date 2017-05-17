c0981a20274b85033950b04c08fdd1fb829e47b2
Jake Samson <owlbear@sleek.home.ryavec.com>
2017-05-16 20:22:20 -0400

    yeah

--- example_binary/Program.cs
+++ example_binary/Program.cs
@@ -5,7 +5,119 @@ namespace example_binary {
   class MainClass {
     public static void Main(string[] args) {
         Console.WriteLine("Hello, WORLD!!!");
-    }
+        int j = 5;
+        int i = 6;
+        Console.WriteLine(i * j);
+        j = 6;
+        i = 7;
+        Console.WriteLine(i / j);
+        float dude = 6;
+        float dude2 = 8;
+        Console.WriteLine (dude / dude2);
+        bool seer = true;
+        string yarn = "Some String";
+        Console.WriteLine(yarn);
+        string rope = "Hempen";
+        Console.WriteLine(yarn + rope);
+        int x = 20;
+        int y = 40;
+        int z = 60;
+        Console.WriteLine(x + y + z);
+        float newt = 10;
+        float toad = 9;
+        Console.WriteLine(toad / newt);
+        string hey1 = "uuuummmmmm.....";
+        string hey2 = "He ";
+        string hey3 = "is ";
+        string hey4 = "a total douche, ";
+        string hey5 = "like,";
+        string hey6 = "Whatever";
+        Console.WriteLine(hey1 + hey2 + hey3 + hey4 + hey5 + hey6);
+        int a = 15;
+        int b = 14;
+        int c = 13;
+        Console.WriteLine(a * b * c);
+        float ankh = 1000;
+        float derbun = 300;
+        float ezrak = 17;
+        Console.WriteLine(ankh * derbun / ezrak);
+        string rock30 = "You think I wanna be on stage with a man who recorded an anti condom PSA?";
+        Console.WriteLine(rock30);
+        string rock31 = "I SAVED A LOT OF KIDS FROM LAME SEX!!!!";
+        Console.WriteLine(rock31);
+        int d = 405;
+        int e = 140;
+        int f = 390;
+        int g = 406;
+        int h = 387;
+        Console.WriteLine(d / e * f + g - h);
+        float adrik = 3198;
+        float dirk = 2039;
+        float verrian = 20987;
+        Console.WriteLine(verrian / dirk * adrik);
+        string fid = "Woah there";
+        Console.WriteLine(fid);
+        string fid1 = "Hey ther doge";
+        Console.WriteLine(fid1);
+        int k = 1038;
+        int l = 917356;
+        int m = 937615;
+        int n = 35976;
+        int o = 7912350;
+        int p = 32947677;
+        Console.WriteLine(l - m * n + o * p);
+        float wee1 = 29476;
+        float weel2 = 48357;
+        Console.WriteLine(weel2 / wee1);
+        x = 500;
+        y = 400;
+        z = 600;
+        Console.WriteLine(x + y + z);
+        Console.WriteLine(a + b + c + x + y + z);
+        string rock32 = "It all starts with words!!!!";
+        Console.WriteLine(rock32);
+
+        Console.WriteLine("Some string {0} an {1} goes there {2}", x, "WHATEVER", dirk);
+        Console.Write("Some string ");
+        Console.Write(x);
+        Console.Write(" and ");
+
+        Console.WriteLine(5);
+        Console.WriteLine("Sum sting {3} and {2} an {0} end {1}", wee1, p, rock32, a);
+        Console.Write("erlo");
+        Console.WriteLine("weyoooooooooooooooooo");
+
+
+        int q = 1454;
+        int r = 9746;
+        int s = 3247;
+        int t = 9237;
+        int u = 4576;
+        Console.WriteLine(r - s * t + u / q);
+
+        float ruby = 3245;
+        float sapphire = 4365;
+        float emerald = 2394324343;
+        Console.WriteLine(ruby * sapphire / emerald);
+
+
+        Console.WriteLine("Some special string.");
+
+        float gold = 8945;
+        float silver = 6574;
+        float crystal = 23;
+        Console.WriteLine(gold * silver / crystal);
+
+
+        int v = 1233;
+        int w = 4587;
+        Console.WriteLine(w * v * u * t * s * r * q);
+
+
+
+
+
+          }
   }
 
 }
